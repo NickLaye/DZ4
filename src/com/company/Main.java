a85ef3893594ff0bb0bda8c827258bb01e523c06
NickLaye <nicklaye@mail.ru>
2022-04-06 13:49:04 +0300

    Гщтовое ДЗ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        // Задача 1
+
+        int i = 1;
+        while (i<= 10) {
+            System.out.print(i++ + " ");
+        }
+        System.out.print("\n");
+        for (i = 10;i >= 1; i = i - 1) {
+            System.out.print(i + " ");
+        }
+        System.out.print("\n");
+
+        // Задача 2
+
+        int friday = 3;
+        for (; friday < 31; friday = friday +7) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+        }
+
+        // Задание 3
+
+        int a = 1817;
+        for (; a <= 2117; a = a + 79) {
+            if (a > 1817)
+            System.out.println(a);
+        }
+
+
     }
 }
